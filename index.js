91f62c2a69608d3f89e3085374996dd413e950b6
kkmusau <kkmusau@gmail.com>
2022-06-17 18:58:57 -0500

    added questions for employees on questions.js and markup html in index.html

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-// required to link to create html page
+//node modules
+const fs = require('fs');
+const { prompt } = require ('inquirer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const questions = require('./src/questions');
 const generateHTML = require('./src/generateHTML');
+
+const employees = {Manager:[],Intern:[],Engineer:[]};
+
+init();
+
+function init() {
+    prompt (questions).then(ans => {
+        if (ans.isEmployee) {
+            if (ans.role === "Manager"){
+                employees.Manager.push(new Manager(ans.name, ans.id, ans.email, ans.officeNumber));
+            };
+            if (ans.role === "Engineer"){
+                employees.Engineer.push(new Engineer(ans.name, ans.id, ans.email, ans.github));
+            };
+            if (ans.role === "Intern"){
+                employees.Intern.push(new Intern(ans.name, ans.id, ans.email, ans.school));
+            };
+
+            init();    
+        } else {
+            generateHTML(employees);
+        }
+    });
+}
+
---
+++ src/questions.js
@@ -0,0 +1,52 @@
+const questions =     [
+    {
+        type: 'confirm',
+        name: 'isEmployee',
+        message: 'Would you like to add and employee?'
+    },
+    {
+        type: 'list',
+        name: 'role',
+        message: 'Which position would you like to fill?',
+        choices: ['Manager','Engineer','Intern'],
+        when: ({ isEmployee }) => isEmployee
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the emplyee\'s name?',
+        when: ({ isEmployee }) => isEmployee
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the emplyee\'s id?',
+        when: ({ isEmployee }) => isEmployee
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the emplyee\'s email address?',
+        when: ({ isEmployee }) => isEmployee
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'What is the manager\'s office number?',
+        when: ({ role }) => role === 'Manager'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is the engineer\'s Github username?',
+        when: ({ role }) => role === 'Engineer'
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'What is the intern\'s school?',
+        when: ({ role }) => role === 'Intern'
+    },
+]
+
+module.exports= questions;
